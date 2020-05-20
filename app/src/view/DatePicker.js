1227ed7bb4a2a502b1b4ea6cd13b1a5b75b9ee77
Jody McIntyre <scjody@modernduck.com>
2020-05-20 16:27:30 -0400

    :camel: borderColor and vary it based on theme
    Colours from https://github.com/mui-org/material-ui/blob/bf8f3f7d35114e2df9b9836ae663187c7d65e608/packages/material-ui/src/OutlinedInput/OutlinedInput.js#L10-L11

--- app/src/view/DatePicker.js
+++ app/src/view/DatePicker.js
@@ -18,64 +18,68 @@ type P = {
   label: string,
 };
 
-const useStyles = makeStyles(theme => ({
-  label: {
-    position: 'relative',
-    display: 'inline-block',
-    paddingTop: '6px',
-    marginTop: '16px',
-    marginBottom: '8px',
-  },
-  datepicker: {
-    backgroundColor: theme.palette.background.paper,
-    borderColor: 'rgba(255, 255, 255, 0.23)',
-    borderTopColor: 'transparent',
-    border: 'solid 1px',
-    borderRadius: theme.shape.borderRadius,
-    padding: '18.5px 14px',
-    color: theme.palette.text.primary,
-    fontSize: theme.typography.fontSize,
-    lineHeight: '18px',
-    '&:hover': {
-      borderColor: theme.palette.text.primary,
+const useStyles = makeStyles((theme) => {
+  const borderColor = theme.palette.type === 'light' ? 'rgba(0, 0, 0, 0.23)' : 'rgba(255, 255, 255, 0.23)';
+
+  return {
+    label: {
+      position: 'relative',
+      display: 'inline-block',
+      paddingTop: '6px',
+      marginTop: '16px',
+      marginBottom: '8px',
+    },
+    datepicker: {
+      backgroundColor: theme.palette.background.paper,
+      borderColor: borderColor,
       borderTopColor: 'transparent',
+      border: 'solid 1px',
+      borderRadius: theme.shape.borderRadius,
+      padding: '18.5px 14px',
+      color: theme.palette.text.primary,
+      fontSize: theme.typography.fontSize,
+      lineHeight: '18px',
+      '&:hover': {
+        borderColor: theme.palette.text.primary,
+        borderTopColor: 'transparent',
+      },
     },
-  },
-  span: {
-    position: 'absolute',
-    top: '0',
-    left: '0',
-    display: 'flex',
-    width: '100%',
-    fontSize: '10px',
-    lineHeight: '15px',
-    '&:before,&:after': {
-      content: '""',
-      display: 'block',
-      boxSizing: 'border-box',
-      marginTop: '6px',
-      borderTop: 'solid 1px',
-      borderColor: 'rgba(255, 255, 255, 0.23)',
-      minWidth: '10px',
-      height: '8px',
+    span: {
+      position: 'absolute',
+      top: '0',
+      left: '0',
+      display: 'flex',
+      width: '100%',
+      fontSize: '10px',
+      lineHeight: '15px',
+      '&:before,&:after': {
+        content: '""',
+        display: 'block',
+        boxSizing: 'border-box',
+        marginTop: '6px',
+        borderTop: 'solid 1px',
+        borderColor: borderColor,
+        minWidth: '10px',
+        height: '8px',
+      },
+      '&:hover::before,&:hover::after': {
+        borderColor: theme.palette.text.primary,
+      },
+      '&:before': {
+        marginRight: '4px',
+        borderLeft: 'solid 1px transparent',
+        borderRadius: '4px 0',
+      },
+      '&:after': {
+        flexGrow: '1',
+        marginLeft: '4px',
+        borderRight: 'solid 1px transparent',
+        borderRadius: '0 4px 4px 0',
+        height: '57px',
+      },
     },
-    '&:hover::before,&:hover::after': {
-      borderColor: theme.palette.text.primary,
-    },
-    '&:before': {
-      marginRight: '4px',
-      borderLeft: 'solid 1px transparent',
-      borderRadius: '4px 0',
-    },
-    '&:after': {
-      flexGrow: '1',
-      marginLeft: '4px',
-      borderRight: 'solid 1px transparent',
-      borderRadius: '0 4px 4px 0',
-      height: '57px',
-    },
-  },
-}));
+  };
+});
 
 const DatePicker = ({name, onChange, selected, label}: P) => {
   const onChangeWrapper = (value) => {
